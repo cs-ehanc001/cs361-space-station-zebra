e9fd537b00ce5367fac4ab4b57e0547c2b8b2c4e
theomegacarrot <theomegacarrot@gmail.com>
2022-09-21 21:17:35 -0400

    ship basics set

--- inc/ship.h
+++ inc/ship.h
@@ -1,6 +1,8 @@
 #ifndef SHIP_H
 #define SHIP_H
 
+#include <numeric>
+
 #include "constants.h"
 #include "list.hpp"
 
@@ -8,6 +10,10 @@ namespace zebra {
 
 class ship
 {
+public:
+
+  enum class faction { human, ferengi, klingon, romulan, other };
+
 private:
 
   struct part {
@@ -17,11 +23,25 @@ private:
 
   int m_id;
 
-  ehanc::list<part> m_parts;
+  faction m_faction;
+
+  ehanc::list<part> m_damaged_parts;
+
+  void p_create_damage();
 
 public:
 
-  ship() noexcept;
+  ship() = delete;
+
+  ship(faction fact)
+      : m_id {}
+      , m_faction {fact}
+      , m_damaged_parts {get_damaged_part_list(fact)}
+  {
+    static int next_id {conf::starting_ship_id};
+    m_id = next_id;
+    ++next_id;
+  }
 
   ship(const ship& src) noexcept;
 
@@ -31,7 +51,45 @@ public:
 
   auto operator=(ship&& rhs) -> ship& noexcept;
 
-  virtual ~ship() noexcept = default;
+  ~ship() noexcept = default;
+
+  /* {{{ doc */
+  /**
+   * @brief Get sum of all damage values of all parts.
+   */
+  /* }}} */
+  inline auto get_total_damage() const noexcept -> int
+  {
+    if ( this->is_damaged() ) {
+      return std::accumulate(m_damaged_parts.cbegin(),
+                             m_damaged_parts.cend(), 0,
+                             [](int acc, const part arg) noexcept {
+                               return acc + arg.damage;
+                             });
+    } else {
+      return 0;
+    }
+  }
+
+  /* {{{ doc */
+  /**
+   * @brief Determines if ship is damaged.
+   */
+  /* }}} */
+  inline auto is_damaged() const noexcept -> bool
+  {
+    return m_damaged_parts.size() == 0;
+  }
+
+  /* {{{ doc */
+  /**
+   * @brief Clears list of damaged parts.
+   */
+  /* }}} */
+  inline void repair() noexcept
+  {
+    m_damaged_parts.clear();
+  }
 };
 
 } // namespace zebra
